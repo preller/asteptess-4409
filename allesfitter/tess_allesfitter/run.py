59b55e4b2b717a0a9848abd1c5ed0e862036daed
Patricio Yael Reller <patricioyr@gmail.com>
2023-10-31 10:14:32 +0100

    Added 1 hour TTVs fit

--- allesfitter/tess_allesfitter/run.py
+++ allesfitter/tess_allesfitter/run.py
@@ -29,6 +29,11 @@ os.chdir(dir_path)
 
 # For TTV fits:
 # allesfitter.prepare_ttv_fit('allesfit_with_ttvs')
-allesfitter.show_initial_guess('allesfit_with_ttvs')
-allesfitter.ns_fit('allesfit_with_ttvs')
-allesfitter.ns_output('allesfit_with_ttvs')
+# allesfitter.show_initial_guess('allesfit_with_ttvs')
+# allesfitter.ns_fit('allesfit_with_ttvs')
+# allesfitter.ns_output('allesfit_with_ttvs')
+
+# with TTVS = b_ttv_transit_1,0,1,uniform -0.04167 0.04167,TTV$_\mathrm{b;1}$,d,
+allesfitter.show_initial_guess('allesfit_with_ttvs_1hour')
+allesfitter.ns_fit('allesfit_with_ttvs_1hour')
+allesfitter.ns_output('allesfit_with_ttvs_1hour')
